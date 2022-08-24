b8cfb0b8e159fadfaef41dafd2fd0758d11f3963
Pablo Szejpiacki <molder26@hotmail.com>
2022-08-24 09:56:19 -0300

    Move Browserrouter to app.js where router are definied

--- front/src/App.js
+++ front/src/App.js
@@ -1,28 +1,29 @@
-import { Route } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom";
 import LandingPage from "./Components/LandingPage";
 import "./App.css";
 
 function App() {
-  return (
-    <>
-      {/*Se agregan las rutas para el primer spint solo queda importar los componentes y pasarlos a cada ruta, puede ser necesario realizar cambios, ir descomentando una a una para que se puedan renderizar bien los componentes */}
+    return (
+        // Se agregan las rutas para el primer spint solo queda importar los componentes y pasarlos a cada ruta, puede ser necesario realizar cambios, ir descomentando una a una para que se puedan renderizar bien los componentes
 
-      {/*Ruta Landing Page */}
-      <Route exact path={"/"} component={LandingPage} />
+        // Ruta Landing Page
 
-      {/*Ruta Catalogo de Libros */}
-      {/* <Route path={"/books"} component={} /> */}
+        <BrowserRouter>
+            <Route exact path={"/"} component={LandingPage} />
+        </BrowserRouter>
 
-      {/*Ruta Detalles de Libro */}
-      {/* <Route path={"/books/detail/:id"} component={} /> */}
+        // Ruta Catalogo de Libros
+        // <Route path={"/books"} component={} />
 
-      {/*Ruta Inicio de Sesion */}
-      {/* <Route path={"/login"} component={} /> */}
+        // Ruta Detalles de Libro
+        // <Route path={"/books/detail/:id"} component={} />
 
-      {/*Ruta administrador*/}
-      {/* <Route path={"/user/admin"} component={} /> */}
-    </>
-  );
+        // Ruta Inicio de Sesion
+        // <Route path={"/login"} component={} />
+
+        // Ruta administrador
+        // <Route path={"/user/admin"} component={} />
+    );
 }
 
 export default App;
