a05f6e4a26d2decb59c117bc3c09cd5ce3f36d26
Pablo Szejpiacki <molder26@hotmail.com>
2022-09-03 11:40:30 -0300

    fix List of favorites after deleted one1

--- front/src/Components/Home/Home.jsx
+++ front/src/Components/Home/Home.jsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { getAllBooks, setPage } from "../../actions/index.js";
-
+import { getAllBooks, getAllFavorites, setPage } from "../../actions/index.js";
 
 //COMPONENTES
 import NavBar from "../NavBar/NavBar.jsx";
@@ -20,85 +19,88 @@ import banner from "./banner.jpg";
 import Loading from "../Loading/Loading.jsx";
 
 export default function Home() {
-  const dispatch = useDispatch();
-  const { actualPage, allBooks, section } = useSelector((state) => state);
+    const dispatch = useDispatch();
+    const { actualPage, allBooks, section, favorites } = useSelector((state) => state);
 
-  const location = useLocation();
-  const search = location.state ? location.state.search : null;
-  const itemsPorPagina = 12;
-  const offset = actualPage * itemsPorPagina;
-  const limit = offset + itemsPorPagina;
+    const location = useLocation();
+    const search = location.state ? location.state.search : null;
+    const itemsPorPagina = 12;
+    const offset = actualPage * itemsPorPagina;
+    const limit = offset + itemsPorPagina;
 
-  useEffect(() => {
-    if (section === "home") {
-      dispatch(getAllBooks());
-      dispatch(setPage(0));
-    }
-  }, [dispatch, section]);
+    useEffect(() => {
+        if (section === "home") {
+            dispatch(getAllBooks());
+            dispatch(setPage(0));
+        } else if (section === "favoritos") {
+            dispatch(getAllFavorites());
+            // dispatch(setPage(0));
+        }
+    }, [dispatch, section, favorites]);
 
-  // useEffect(() => {
+    // useEffect(() => {
 
-  // }, [actualPage])
+    // }, [actualPage])
 
-  // console.log(allBooks, actualPage);
+    // console.log(allBooks, actualPage);
 
-  const currentBooks = allBooks.length > 0 && allBooks.slice(offset, limit);
+    const currentBooks = allBooks.length > 0 && allBooks.slice(offset, limit);
 
-  return (
-    <div className={styles.home}>
-      <NavBar />
-      <NavBar2 />
+    return (
+        <div className={styles.home}>
+            <NavBar />
+            <NavBar2 />
 
-      {currentBooks.length > 0 ? (
-        <>
-          <div className={styles.banner}>
-            <img src={banner} alt="banner" />
-          </div>
+            {currentBooks.length > 0 ? (
+                <>
+                    <div className={styles.banner}>
+                        <img src={banner} alt="banner" />
+                    </div>
 
-          {/* ORDENAMIENTOS */}
-          <div className={styles.ordenamientos}>
-            <Order />
-          </div>
+                    {/* ORDENAMIENTOS */}
+                    <div className={styles.ordenamientos}>
+                        <Order />
+                    </div>
 
-          <div className={styles.paginado}>
-            <Paginated
-              totalItems={allBooks.length}
-              itemsPorPagina={itemsPorPagina}
-            />
-          </div>
+                    <div className={styles.paginado}>
+                        <Paginated
+                            totalItems={allBooks.length}
+                            itemsPorPagina={itemsPorPagina}
+                        />
+                    </div>
 
-          <div className={styles.cuerpo}>
-            <div className={styles.filtro}>
-              <CategoryFilter />
-              <AuthorFilter />
-            </div>
+                    <div className={styles.cuerpo}>
+                        <div className={styles.filtro}>
+                            <CategoryFilter />
+                            <AuthorFilter />
+                        </div>
 
-            <div className={styles.cards}>
-              {currentBooks &&
-                currentBooks.map((b) => (
-                  <Book
-                    key={b.id}
-                    id={b.id}
-                    title={b.title}
-                    authors={b.authors}
-                    image={b.image}
-                    price={b.price}
-                    stock={b.currentStock}
-                    allBooks={allBooks}
-                  />
-                ))}
-            </div>
-          </div>
-        </>
-      ) : allBooks.message ? (
-        <div className={styles.ErrorSearch}>
-          <h3 className={styles.errorH3}>{allBooks.message}</h3>
+                        <div className={styles.cards}>
+                            {currentBooks &&
+                                currentBooks.map((b) => (
+                                    <Book
+                                        key={b.id}
+                                        id={b.id}
+                                        title={b.title}
+                                        authors={b.authors}
+                                        image={b.image}
+                                        price={b.price}
+                                        stock={b.currentStock}
+                                        allBooks={allBooks}
+                                    />
+                                ))}
+                        </div>
+                    </div>
+                </>
+            ) : allBooks.message ? (
+                <div className={styles.ErrorSearch}>
+                    <h3 className={styles.errorH3}>{allBooks.message}</h3>
+                </div>
+            ) : (
+                <Loading />
+            )}
+
+            <Footer />
         </div>
-      ) : (
-        <Loading />
-      )}
-
-      <Footer />
-    </div>
-  );
+    );
 }
