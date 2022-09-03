6141cbb59163880aad76d1689224798ed4aa4966
hcamusso <hcamusso@gmail.com>
2022-09-03 07:44:42 -0300

    Agregar useMemo a import de book

--- front/src/Components/Book/Book.jsx
+++ front/src/Components/Book/Book.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { NavLink } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { addFavoriteBook, getAllBooks, setPage, setSection } from "../../actions/index.js";
