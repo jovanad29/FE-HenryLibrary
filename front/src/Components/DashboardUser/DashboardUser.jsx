b314b4d0b681785de123980a0c077cedee9e4edf
Flori O <floroldani@hotmail.com>
2022-09-11 20:51:06 -0300

    maquetado del Panel de usuario

--- front/src/Components/DashboardUser/DashboardUser.jsx
+++ front/src/Components/DashboardUser/DashboardUser.jsx
@@ -1,26 +1,51 @@
-import React from 'react'
-import NavBar from "../NavBar/NavBar.jsx"
-import NavBar2 from "../NavBar2/NavBar2.jsx"
-import Footer from "../Footer/Footer.jsx"
+import React from "react";
+import NavBar from "../NavBar/NavBar.jsx";
+import NavBar2 from "../NavBar2/NavBar2.jsx";
+import Footer from "../Footer/Footer.jsx";
 
-
-
+//CSS
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import styles from "./DashboardUser.module.css";
 
 function DashboardUser() {
   return (
+    <div className={styles.dashboardUser}>
+      <NavBar />
+      <NavBar2 />
 
-    <div>
-        <NavBar />
-        <NavBar2 />
+      <div className={styles.container}>
+        <Tabs variant="soft-rounded" colorScheme="green">
+          <TabList>
+            <Tab >Datos Personales</Tab>
+            <Tab>Direcciones</Tab>
+            <Tab>Ordenes de compras</Tab>
+            <Tab>Historial de Comentarios</Tab>
+            <Tab>Seguridad</Tab>
+          </TabList>
+          <TabPanels className={styles.containerItem}>
+            <TabPanel >
+              <div>Datos personales</div>
+            </TabPanel>
+            <TabPanel>
+            <div>Direcciones</div>
+            </TabPanel>
+            <TabPanel>
+              <p>Ordenes de compras</p>
+            </TabPanel>
+            <TabPanel>
+              <p>Historial de Comentario</p>
+            </TabPanel>
+            <TabPanel>
+            <div>Seguridad</div>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </div>
 
-        <div>
-
-        </div>
-
-        <Footer/>
-
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default DashboardUser
+export default DashboardUser;
+
