87a1ea28b8234eaa423dd54d09d0edd263d41a73
Flori O <floroldani@hotmail.com>
2022-09-16 21:13:38 -0300

    Reacomodo de funcion validate y rearreglo de la validacion del post de review

--- front/src/Components/NewBook/validate.js
+++ front/src/Components/NewBook/validate.js
@@ -56,16 +56,3 @@ export default function validate(input) {
   return errores;
 }
 
-export function validateReview(input) {
-  let errores = {};
-
-  if (!input.descrption) {
-    errores.descrption = "Se requiere una descripcion";
-  } else if (input.descrption.length > 100) {
-    errores.descrption = "supero el maximo de 100 caracteres";
-  }else if (!/^[a-zA-ZñÑáéíóúÁÉÍÓÚ]+$/.test(input.descrption)){
-    errores.descrption = "Debe escribir un mensaje";
-  }
-
-  return errores;
-}
