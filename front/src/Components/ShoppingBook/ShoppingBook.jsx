6710179f0db49fda0cf0618fa36047923c387b8f
Pablo Szejpiacki <molder26@hotmail.com>
2022-09-17 00:15:02 -0300

    clean code commented

--- front/src/Components/ShoppingBook/ShoppingBook.jsx
+++ front/src/Components/ShoppingBook/ShoppingBook.jsx
@@ -162,61 +162,6 @@ function ShoppingBook() {
                 handleChangeQuantity={handleChangeQuantity}
             />
         );
-
-        // return (
-        //     <div className={styles.item} key={id}>
-        //         <img
-        //             className={styles.img}
-        //             src={image}
-        //             alt=""
-        //             width={10}
-        //             heigh={10}
-        //         />
-
-        //         <div className={styles.info}>
-        //             <div className={styles.infoItem1}>
-        //                 <h3 className={styles.title}>{title}</h3>
-        //             </div>
-        //             <div className={styles.infoItem2}>
-        //                 <h2 className={styles.precio}>
-        //                     Precio: $ {parseFloat(price).toFixed(2)}
-        //                 </h2>
-        //                 <div className={styles.cantidadContainer}>
-        //                     <label className={styles.cantidad}>
-        //                         Cantidad:{" "}
-        //                     </label>
-        //                     <BiMinus />
-        //                     <input
-        //                         type="number"
-        //                         className={styles.cantidadInput}
-        //                         defaultValue={quantity}
-        //                         min={"1"}
-        //                         max={"100"}
-        //                         onChange={(e) =>
-        //                             handleChangeQuantity(
-        //                                 e,
-        //                                 id,
-        //                                 price,
-        //                                 currentStock
-        //                             )
-        //                         }
-        //                     />
-        //                     <BsPlus />
-        //                     {/*quantity*/}
-        //                 </div>
-        //                 <h2 className={styles.precio}>
-        //                     Total: $ {parseFloat(price * quantity).toFixed(2)}
-        //                 </h2>
-        //                 <button
-        //                     className={styles.itemCancelar}
-        //                     onClick={() => handleOnDelete(id, 0, 0)}
-        //                 >
-        //                     <FaTrashAlt color="#01A86C" size="1rem" />
-        //                 </button>
-        //             </div>
-        //         </div>
-        //     </div>
-        // );
     });
 
     const totalBooks = total.totalBooks;
