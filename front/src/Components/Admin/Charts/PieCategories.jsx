c08c4b20463127a6f147c268d30823e0d48d56f0
YepesF <yepilef@gmail.com>
2022-09-19 20:11:34 -0500

    Despachar la accion para obtener categorias mas vendidas

--- front/src/Components/Admin/Charts/PieCategories.jsx
+++ front/src/Components/Admin/Charts/PieCategories.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Pie } from "react-chartjs-2";
 import { Box } from "@chakra-ui/react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getCategoriesMostBuy } from "../../../actions/dashboardActions";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 function PieCategories() {
+  const dispatch = useDispatch();
+  const { categoriesMostBuies } = useSelector((estate) => estate);
   const data = {
     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
     datasets: [
@@ -33,8 +38,13 @@ function PieCategories() {
     ],
   };
 
+  useEffect(() => {
+    dispatch(getCategoriesMostBuy);
+  }, [dispatch]);
+
   return (
     <Box ml="18%" w="30%" fontFamily="Quicksand">
+      {console.log(categoriesMostBuies)}
       <Pie data={data} />
     </Box>
   );
