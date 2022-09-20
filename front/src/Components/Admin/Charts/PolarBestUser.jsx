504dd110a8aa8f6abf8f3c358b62259f1a6cc7c0
YepesF <yepilef@gmail.com>
2022-09-20 11:34:31 -0500

    Crear flow para traer los datos necesarios

--- front/src/Components/Admin/Charts/PolarBestUser.jsx
+++ front/src/Components/Admin/Charts/PolarBestUser.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   Chart as ChartJS,
   RadialLinearScale,
@@ -8,37 +8,53 @@ import {
 } from "chart.js";
 import { PolarArea } from "react-chartjs-2";
 import { Box } from "@chakra-ui/react";
-
-ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
+import { useDispatch, useSelector } from "react-redux";
+import { getUsersMostBuy } from "../../../actions/dashboardActions";
 
 function PolarBestUser() {
-  const data = {
-    labels: [
-      "Usuario1",
-      "Usuario2",
-      "Usuario3",
-      "Usuario4",
-      "Usuario5",
-      "Usuario6",
-    ],
-    datasets: [
-      {
-        data: [50, 48, 30, 25, 12, 3],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.5)",
-          "rgba(54, 162, 235, 0.5)",
-          "rgba(255, 206, 86, 0.5)",
-          "rgba(75, 192, 192, 0.5)",
-          "rgba(153, 102, 255, 0.5)",
-          "rgba(255, 159, 64, 0.5)",
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
+  const dispatch = useDispatch();
+  const { usersMostBuies } = useSelector((estate) => estate);
+
+  const [usersData, setUsersData] = useState({ datasets: [] });
+
+  useEffect(() => {
+    dispatch(getUsersMostBuy());
+  }, [dispatch]);
+
+  useEffect(() => {
+    const data = usersMostBuies.splice(0, 5);
+    setUsersData({
+      labels: data.map((d) => d.nameUser),
+      datasets: [
+        {
+          data: data.map((d) => d.totalBookBuy),
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    });
+  }, [usersMostBuies]);
+
+  ChartJS.register(RadialLinearScale, ArcElement, Tooltip, Legend);
+
   return (
-    <Box ml="18%" w="30%" fontFamily="Segoe UI">
-      <PolarArea data={data} />
+    <Box ml="18%" w="30%" fontFamily="Quicksand">
+      <PolarArea data={usersData} />
     </Box>
   );
 }
