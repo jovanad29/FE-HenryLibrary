3f9f07a0b40af500a1e86d12f513343da385d12b
Flori O <floroldani@hotmail.com>
2022-09-19 23:52:26 -0300

    claro/oscuro de filtros

--- front/src/Components/Home/Home.jsx
+++ front/src/Components/Home/Home.jsx
@@ -212,7 +212,7 @@ export default function Home() {
                                 finalFocusRef={btnRef}
                             >
                                 <DrawerOverlay />
-                                <DrawerContent>
+                                <DrawerContent className={styles.fondoFiltro}>
                                     <Flex pt={"10%"} justifyContent={"center"}>
                                         <Button
                                             ref={btnRef}
