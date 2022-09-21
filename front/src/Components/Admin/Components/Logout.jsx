03522e43c86b2026770b993f8bd9fd8f6e8a24da
YepesF <yepilef@gmail.com>
2022-09-20 19:07:07 -0500

    Corregir tamaño del boton de logout

--- front/src/Components/Admin/Components/Logout.jsx
+++ front/src/Components/Admin/Components/Logout.jsx
@@ -5,18 +5,18 @@ import { useHistory } from "react-router-dom";
 import { startLogout } from "../../../actions";
 
 function Logout() {
-    const dispatch = useDispatch();
-    const history = useHistory();
-    const handleCloseSesion = () => {
-      dispatch(startLogout());
-    };
-    return (
-        <Box>
-            <Button colorScheme="green" size="sm" onClick={handleCloseSesion}>
-                Logout
-            </Button>
-        </Box>
-    );
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const handleCloseSesion = () => {
+    dispatch(startLogout());
+  };
+  return (
+    <Box>
+      <Button background="#01A86C" size="lg" onClick={handleCloseSesion}>
+        Logout
+      </Button>
+    </Box>
+  );
 }
 
 export default Logout;
