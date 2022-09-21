d4798a68ae305db98926e011facbba52ac9a4165
Jovana Davalillo <jovana.davalillo@gmail.com>
2022-09-21 17:21:48 -0400

    Fix spacing

--- front/src/Components/MercadoPago/SuccessMP.js
+++ front/src/Components/MercadoPago/SuccessMP.js
@@ -53,93 +53,92 @@ export default function SuccessMP() {
     dispatch(getCartDB(uid));
     history.push("/home");
   }
-  return (
-    <>
-      {order.items.length > 0 ? (
-        <div className={s.container}>
-         
-          <div className={s.cont}>
-            <div className={s.contGreen}>
-              <div className={s.check}>
-                <BsCheckCircle fontSize="6rem" />
-              </div>
-              <h1 className={s.titulo}>{t("exito")}</h1>
-              <div className={s.transaccion}>
-                {t("transaccion")}:{" "}
-                <span className={s.pID}>{order.transactionId}</span>
-              </div>
-              {/* <div className={s.transaccion}>
-                {t("status")}: <span className={s.pID}>{order.status}</span>
-              </div> */}
-              <span className={s.itemsTotales}>
-                Total items: {activeCartQuantity}
-              </span>{" "}
-              <TableContainer className={s.tabla}>
-                <Table
-                  key={Math.random()}
-                  variant="striped"
-                  colorScheme="green"
-                >
-                  <Thead>
-                    <Tr>
-                      <Th className={s.tituloTabla}>{t("libro")}</Th>
-                      <Th isNumeric className={s.tituloTabla}>
-                      {t("qty")}
-                      </Th>
-                      <Th isNumeric className={s.tituloTabla}>
-                      {t("precio")}
-                      </Th>
-                    </Tr>
-                  </Thead>
-                  <Tbody>
-                    {order.withDelivery.map((i) => {
-                      return (
-                        <Tr>
-                          <Td>{i.title}</Td>
-                          <Td isNumeric>
-                            {i.bookId !== 0 && i.description !== "Retira en Sucursal"
-                              ? i.quantity
-                              : " "}
-                          </Td>
-                          <Td isNumeric>
-                            {i.bookId !== 0 && i.description !== "Retira en Sucursal"
-                              ? i.price
-                              : " "}
-                          </Td>
-                        </Tr>
-                      );
-                    })}
-                  </Tbody>
-                </Table>
-              </TableContainer>
-              <span className={s.totalLibros}>
-                Total {t("libros")}:{" "}
-                <span className={s.price}> ${activeCartAmount}</span>
-              </span>
-              <span className={s.total}>
-                Total:{" "}
-                <span className={s.price}>
-                  {" "}
-                  ${parseFloat(order.total).toFixed(2)}
-                </span>
-              </span>
-              <Button className={s.boton} onClick={goBack}>
-                {t("continuarComprando")}
-              </Button>
-              <div className={s.successCheckmark}>
-                <div className={s.checkIcon}>
-                  <span className={`${s.iconLine} ${s.lineTip}`}></span>
-                  <span className={`${s.iconLine} ${s.lineLong}`}></span>
-                  <div className={s.iconCircle}></div>
-                  <div className={s.iconFix}></div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <Loading />
-      )}
-    </>
-  );
+	return (
+		<>
+			{order.items.length > 0 ? (
+			<div className={s.container}>				
+				<div className={s.cont}>
+					<div className={s.contGreen}>
+						<div className={s.check}>
+							<BsCheckCircle fontSize="6rem" />
+						</div>
+						<h1 className={s.titulo}>{t("exito")}</h1>
+						<div className={s.transaccion}>
+							{t("transaccion")}:{" "}
+							<span className={s.pID}>{order.transactionId}</span>
+						</div>
+						<div className={s.transaccion}>
+							{t("status")}: <span className={s.pID}>{order.status}</span>
+						</div>
+						<span className={s.itemsTotales}>
+							Total items: {activeCartQuantity}
+						</span>{" "}
+						<TableContainer className={s.tabla}>
+						<Table
+							key={Math.random()}
+							variant="striped"
+							colorScheme="green"
+						>
+							<Thead>
+								<Tr>
+									<Th className={s.tituloTabla}>{t("libro")}</Th>
+									<Th isNumeric className={s.tituloTabla}>
+										{t("qty")}
+									</Th>
+									<Th isNumeric className={s.tituloTabla}>
+										{t("precio")}
+									</Th>
+								</Tr>
+							</Thead>
+							<Tbody>
+								{order.withDelivery.map((i) => {
+									return (
+									<Tr>
+										<Td>{i.title}</Td>
+										<Td isNumeric>
+											{i.bookId !== 0 && i.description !== "Retira en Sucursal"
+												? i.quantity
+												: " "}
+										</Td>
+										<Td isNumeric>
+											{i.bookId !== 0 && i.description !== "Retira en Sucursal"
+												? i.price
+												: " "}
+										</Td>
+									</Tr>
+									);
+								})}
+							</Tbody>
+						</Table>
+						</TableContainer>
+						<span className={s.totalLibros}>
+							Total {t("libros")}:{" "}
+							<span className={s.price}> ${activeCartAmount}</span>
+						</span>
+						<span className={s.total}>
+							Total:{" "}
+							<span className={s.price}>
+								{" "}
+								${parseFloat(order.total).toFixed(2)}
+							</span>
+						</span>
+						<Button className={s.boton} onClick={goBack}>
+							{t("continuarComprando")}
+						</Button>
+						<div className={s.successCheckmark}>
+							<div className={s.checkIcon}>
+								<span className={`${s.iconLine} ${s.lineTip}`}></span>
+								<span className={`${s.iconLine} ${s.lineLong}`}></span>
+								<div className={s.iconCircle}></div>
+								<div className={s.iconFix}></div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			) : (
+			<Loading />
+			)}
+		</>
+	);
 }
